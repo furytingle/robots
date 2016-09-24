dd73fa13039f8e43310fccf62f4063957f5a513c
Zhylinskiy Aleksandr <zhylinskiy.aleksandr@gmail.com>
2016-09-24 11:26:14 +0300

    stage

--- Factory/RobotFactory.php
+++ Factory/RobotFactory.php
@@ -6,6 +6,9 @@ use Interfaces\RobotInterface;
 
 class RobotFactory
 {
+    const CREATE_FUNC = 'create';
+
+    const ROBOTS_NAMESPACE = "Robots\\";
 
     protected $typeList;
 
@@ -17,7 +20,29 @@ class RobotFactory
         $this->typeList[] = get_class($robot);
     }
 
-    public function _call($name, $args) {
-        
+    public function _call($name, $args = null) {
+
+        $typeName = str_replace(self::CREATE_FUNC, '', $name);
+
+        if (null !== $args) {
+            $result = [];
+
+            for ($i = 1; $i < $args[0]; $i++) {
+                $result[] = $this->createRobot($typeName);
+            }
+
+            return $result;
+        } else {
+            return $this->createRobot($typeName);
+        }
+    }
+
+    private function createRobot($typeName) {
+        if (in_array($typeName, $this->typeList)) {
+            $className = self::ROBOTS_NAMESPACE . $typeName;
+            return new $className();
+        } else {
+            throw new \Exception("Unknown robot type, add type before creating");
+        }
     }
 }
